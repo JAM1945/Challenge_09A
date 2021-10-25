1ca3f5290dbc052f4714d012727978119b5fe3a2
Jeff Mund <jeffmund@Jeffs-MacBook-Pro-2.local>
2021-10-24 22:36:39 -0500

    updated javascript coding for readme questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,81 @@
 // TODO: Include packages needed for this application
 
+const fs = require('fs');
+const inquirer = require('inquirer');
+// const readMePage= require('./src/page-template');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ()=>{
+    return inquirer.prompt([
+
+{
+        type: 'input',
+        name: 'projecTitle',
+        message: 'What is your project title?'
+      },
+
+      {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Describe your project?'
+      },
+
+
+      {
+        type: 'input',
+        name: 'applicationSite',
+        message: 'Please provide the link to your application if deployed?'
+      },
+
+      {
+        type: 'input',
+        name: 'installationInstructions',
+        message: 'Please describe any steps to follow in order to install project or software?'
+      },
+
+      {
+        type: 'input',
+        name: 'usageInstructions',
+        message: 'Are there any instructions or examples for usage of your project?'
+      },
+
+      {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'Please list any contributors, collaborators or third party resources utilized in creating project?'
+      },
+
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Have you prepared any test for you application and can you provide examples of test if they exist?'
+      },
+
+      {
+        type: 'checkbox',
+        name: 'licenses',
+        message: 'Licenses that apply?',
+        choices: ['MIT', 'GNU_GPLv3', 'ISC', 'Apache_License_2.0', 'GNU_GPLv2', 'Boost_Software_License2.0', 'Unlicensed']
+      },
+])};
+
+      questions().then(answers=>console.log(answers));
+  
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(, data) {}
+
+
+// □ Includes three arguments:
+// ® Arg. 1:  Name of the file being created
+// ® Arg. 2:  Data to be written into file
+// ® Arg. 3:  Callback function used if there is an error
+// □ Synchronous and Asynchronous versions
+// □ Example:
+fs.writeFile('./README.md', "", err => {
+  if(err) thrownewError(err);
+
 
 // TODO: Create a function to initialize app
 function init() {}
